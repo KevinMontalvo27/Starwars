72b07738ef8671fdad4766a3d917cb66b297149b
KevinMontalvo27 <montalvokevin375@gmail.com>
2025-08-27 11:39:38 -0700

    Agregar estilos a la tabla

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,7 +4,7 @@ export const Table = ({data, selectedCharacter}) => {
 
     
     const columns = ["Nombre", "Altura", "Peso", "Color de cabello", "Color de piel", "Color de ojos"
-    , "Fecha de nacimiento", "Genero", "Planeta de nacimiento", "Mas"];
+    , "Fecha de nacimiento", "Genero", "Planeta de nacimiento", "Ver más información"]; 
     
 
     useEffect(() => {
@@ -15,28 +15,28 @@ export const Table = ({data, selectedCharacter}) => {
 
     return (
         <div className="mx-auto max-w-5xl overflow-x-auto">
-            <table className="min-w-full shadowd-md border-collapse text-left rtl:text-right text-sm bg-gray-400">
-                <thead className="text-xs font-semibold uppercase bg-gray-50">
+            <table className="min-w-full border-collapse text-left text-sm bg-gray-700 text-white">
+                <thead className="bg-gray-800 text-xs font-semibold uppercase">
                     <tr>
                         {columns.map((column) =>(
-                            <th scope="col" className="px-6 py-3" key={column}>{column}</th>
+                            <th scope="col" className="px-4 py-3 border-b border-gray-600" key={column}>{column}</th>
                         ))}
                     </tr>
                 </thead>
                 <tbody >
                     {
                         data.map((character, index) =>(
-                            <tr key={index} className="bg-white border-b  border-gray-200">
-                                <td>{character.name}</td>
-                                <td>{character.height}</td>
-                                <td>{character.mass}</td>
-                                <td>{character.hair_color}</td>
-                                <td>{character.skin_color}</td>
-                                <td>{character.eye_color}</td>
-                                <td>{character.birth_year}</td>
-                                <td>{character.gender}</td>
-                                <td>{character.homeworld}</td>
-                                <td className="text-2xl">+</td>
+                            <tr key={index} className="border-b border-gray-600 bg-gray-700">
+                                <td className="px-4 py-2">{character.name}</td>
+                                <td className="px-4 py-2">{character.height}</td>
+                                <td className="px-4 py-2">{character.mass}</td>
+                                <td className="px-4 py-2">{character.hair_color}</td>
+                                <td className="px-4 py-2">{character.skin_color}</td>
+                                <td className="px-4 py-2">{character.eye_color}</td>
+                                <td className="px-4 py-2">{character.birth_year}</td>
+                                <td className="px-4 py-2">{character.gender}</td>
+                                <td className="px-4 py-2">{character.homeworld}</td>
+                                <td className="text-2xl px-4 py-2 cursor-pointer hover:text-yellow-300" onClick={selectedCharacter}>+</td>
                             </tr>
                         ))
                         
